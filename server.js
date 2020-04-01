9cf931cffe458167389a27b693e10211783bb380
Muhammad Anas <muhd.anas@outlook.com>
2020-04-01 15:17:06 +0800

    Created CRUD routes for bootcamps.

--- server.js
+++ server.js
@@ -5,6 +5,22 @@ dotenv.config({ path: './config/.env' });
 
 const app = express();
 
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+});
+
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Create new bootcamp' });
+});
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+});
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+});
+
 app.listen(
   process.env.PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`)
