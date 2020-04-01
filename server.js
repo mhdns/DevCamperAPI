dec3c91404268b833fa208013853006d7eddaf4b
Muhammad Anas <muhd.anas@outlook.com>
2020-04-01 15:35:27 +0800

    Created routes folder and router for bootcamps. Implemented in server.js with .use method.

--- server.js
+++ server.js
@@ -1,25 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+// Import bootcamps router
+const bootcamps = require('./routes/bootcamps');
+
 dotenv.config({ path: './config/.env' });
 
 const app = express();
 
-app.get('/api/v1/bootcamps', (req, res) => {
-  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
-});
-
-app.post('/api/v1/bootcamps', (req, res) => {
-  res.status(200).json({ success: true, msg: 'Create new bootcamp' });
-});
-
-app.put('/api/v1/bootcamps/:id', (req, res) => {
-  res.status(200).json({ success: true, msg: `Update bootcamp ${req.params.id}` });
-});
-
-app.delete('/api/v1/bootcamps/:id', (req, res) => {
-  res.status(200).json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
-});
+app.use('/api/v1/bootcamps', bootcamps);
 
 app.listen(
   process.env.PORT,
