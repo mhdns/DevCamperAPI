ab1aae998c0ab3692790b7428bf7d7b3f8ad681d
Muhammad Anas <muhd.anas@outlook.com>
2020-04-02 09:34:00 +0800

    Installed mongoose and chalk. db.js for connecting to database. App level error handling.

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
+const chalk = require('chalk');
 const bootcamps = require('./routes/bootcamps');
+const connectDB = require('./config/db');
+
+// Colors
+const info = chalk.yellow.bold;
+const error = chalk.red;
 
 dotenv.config({ path: './config/.env' });
 
+// Connect to DB
+connectDB();
+
+// Create express app
 const app = express();
 
 // Dev logging middleware
@@ -12,10 +22,18 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Bootcamp Routes
 app.use('/api/v1/bootcamps', bootcamps);
 
-app.listen(
+const server = app.listen(
   process.env.PORT,
   // eslint-disable-next-line no-console
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`)
+  console.log(info(`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`))
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(error(`Error: ${err.message}`));
+  // CLose server and exit
+  server.close(() => process.exit(1));
+});
