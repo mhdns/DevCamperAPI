616a4aea40814d6dc8c8ec83a2516147cc69d3ad
Muhammad Anas <muhd.anas@outlook.com>
2020-04-06 15:13:32 +0800

    Updated seeder to add courses automatically.

--- models/Course.js
+++ models/Course.js
@@ -21,7 +21,7 @@ const CourseSchema = new mongoose.Schema({
   minimumSkill: {
     type: String,
     required: [true, 'Please add minimum skill required.'],
-    enum: ['beginner, intermediate', 'advanced']
+    enum: ['beginner', 'intermediate', 'advanced']
   },
   scholarshipAvailable: {
     type: Boolean,
--- seeder.js
+++ seeder.js
@@ -8,6 +8,7 @@ dotenv.config({ path: './config/.env' });
 
 // Load model
 const Bootcamp = require('./models/Bootcamp');
+const Course = require('./models/Course');
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -19,11 +20,13 @@ mongoose.connect(process.env.MONGO_URI, {
 
 // Read JSON files
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'));
+const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8'));
 
 // Import into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
+    await Course.create(courses);
     console.log(chalk.green.inverse('Data Imported...'));
     process.exit();
   } catch (err) {
@@ -35,6 +38,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
     console.log(chalk.red.inverse('Data Deleted...'));
     process.exit();
   } catch (err) {
