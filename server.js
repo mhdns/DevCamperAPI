819d1cf6e2958193e605b42dcc9c71bbef867052
Muhammad Anas <muhd.anas@outlook.com>
2020-04-11 17:19:33 +0800

    CRUD functionality for admins to edit Users.

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const cookieParser = require('cookie-parser');
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
 const auth = require('./routes/auth');
+const users = require('./routes/users');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
@@ -45,6 +46,7 @@ if (process.env.NODE_ENV === 'development') {
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
 app.use('/api/v1/auth', auth);
+app.use('/api/v1/users', users);
 
 // Error handler
 app.use(errorHandler);
