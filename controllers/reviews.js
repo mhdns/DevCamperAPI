25d98e044f1ab26c374f9278f61636c621ae0f3a
Muhammad Anas <muhd.anas@outlook.com>
2020-04-13 17:37:04 +0800

    Delete route for reviews.

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -50,7 +50,7 @@ exports.addReview = asyncHandler(async (req, res, next) => {
 });
 
 // @desc      Update review
-// @route     POST /api/v1/reviews/:id
+// @route     PUT /api/v1/reviews/:id
 // @access    Private/User/Admin
 exports.updateReview = asyncHandler(async (req, res, next) => {
   let review = await Review.findById(req.params.id);
@@ -70,4 +70,24 @@ exports.updateReview = asyncHandler(async (req, res, next) => {
   });
 
   return res.status(200).json({ success: true, data: review });
+});
+
+// @desc      Delete review
+// @route     DELETE /api/v1/reviews/:id
+// @access    Private/User/Admin
+exports.deleteReview = asyncHandler(async (req, res, next) => {
+  const review = await Review.findById(req.params.id);
+
+  if (!review) {
+    return next(new ErrorResponse(`Review with the ID of ${req.params.id} not found.`, 404));
+  }
+
+  // Check if the review belongs to the user, or the user is an admin
+  if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorResponse('Not Authorized.', 401));
+  }
+
+  await review.remove();
+
+  return res.status(200).json({ success: true, data: {} });
 });
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const {
-  getReviews, getReview, addReview, updateReview
+  getReviews, getReview, addReview, updateReview, deleteReview
 } = require('../controllers/reviews');
 
 const Review = require('../models/Review');
@@ -14,6 +14,10 @@ router
   .get(advancedResults(Review, { path: 'bootcamp', select: 'name description' }), getReviews)
   .post(protect, authorize('admin', 'user'), addReview);
 
-router.route('/:id').get(getReview).put(protect, authorize('user', 'admin'), updateReview);
+router
+  .route('/:id')
+  .get(getReview)
+  .put(protect, authorize('user', 'admin'), updateReview)
+  .delete(protect, authorize('user', 'admin'), deleteReview);
 
 module.exports = router;
